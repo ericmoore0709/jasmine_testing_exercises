63c88159b5f5e71f5dee660903303c1764c5cdbf
Eric Moore <ericmoore0709@gmail.com>
2024-02-20 10:51:58 -0800

    added server tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -11,7 +11,23 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
-  afterEach(function() {
-    // teardown logic
+  it('should handle a lack of new server value', () => {
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+    expect(allServers['server' + serverId]).toBeUndefined();
+  });
+
+  it('should confirm TDs added to the table', () => {
+    submitServerInfo();
+    serverNameInput.value = 'George';
+    submitServerInfo();
+
+    expect(serverTbody.children.length).toEqual(2);
+  });
+
+  afterEach(function () {
+    allServers = {};
+    serverNameInput.value = '';
   });
 });
